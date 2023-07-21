88408c7162bce863a6e8bc4831487ce620ddfb4a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-21 23:00:47 +0300

    Update DisplayComponent.js

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,9 +1,7 @@
 function DisplayComponent() {
   return (
-    <div className="main-section">
-      <div className="clock-holder">
-        <div className="stopwatch"></div>
-      </div>
+    <div>
+      <h1>D</h1>
     </div>
   )
 }
