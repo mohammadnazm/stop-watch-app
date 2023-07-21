432228e4c5ba7e0c2e4978da476154ba6f7abe32
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-21 22:58:53 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css"
+import DisplayComponent from "./components/DisplayComponent"
+import BtnComponent from "./components/BtnComponent"
 
 function App() {
   return (
     <div className="main-section">
       <div className="clock-holder">
-        <div className="stopwatch"></div>
+        <div className="stopwatch">
+          <DisplayComponent />
+          <BtnComponent />
+        </div>
       </div>
     </div>
   )
