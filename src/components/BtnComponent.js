ca323586a4057fd661949898298e0ff3928b99f1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-22 16:12:31 +0300

    Update BtnComponent.js

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -3,9 +3,30 @@ import React from "react"
 function BtnComponent(props) {
   console.log("props=", props)
   return (
-    <button className="stopwatch-btn stopwatch-btn-gre" onClick={props.start}>
-      Start
-    </button>
+    <div>
+      {props.status === 0 ? (
+        <button
+          className="stopwatch-btn stopwatch-btn-gre"
+          onClick={props.start}
+        >
+          Start
+        </button>
+      ) : (
+        ""
+      )}
+      {props.status === 1 ? (
+        <div>
+          <button class="stopwatch-btn stopwatch-btn-red" onClick={props.start}>
+            Stop
+          </button>
+          <button class="stopwatch-btn stopwatch-btn-yel" onClick={props.start}>
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   )
 }
 
