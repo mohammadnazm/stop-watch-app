051f4ef8a5201c2140d5fe898f0a2e1bca3cd2b1
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-22 12:34:14 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ function App() {
   const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 })
   const start = () => console.log("Hello")
 
-  var updateMs = time.ms,
-    updateS = time.s,
-    updateM = time.m,
-    updateH = time.h
+  var updatedMs = time.ms,
+    updatedS = time.s,
+    updatedM = time.m,
+    updatedH = time.h
 
   const run = () => {
-    if (updateM === 60) {
-      updateH++
-      updateM = 0
+    if (updatedM === 60) {
+      updatedH++
+      updatedM = 0
     }
-    if (updateS === 60) {
-      updateM++
-      updateS = 0
+    if (updatedS === 60) {
+      updatedM++
+      updatedS = 0
     }
-    if (updateMs === 100) {
-      updateS++
-      updateMs = 0
+    if (updatedMs === 100) {
+      updatedS++
+      updatedMs = 0
     }
-    updateMs++
-    return setTime({ ms: 0, s: 0, m: 0, h: 0 })
+    updatedMs++
+    return setTime({ ms: updatedMs, s: updatedS, m: updatedM, h: updatedH })
   }
 
   return (
