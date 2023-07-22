609f46589842fb162dbf973f5015e1ca5f458017
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-22 11:20:31 +0300

    Update DisplayComponent.js

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -4,11 +4,14 @@ function DisplayComponent(props) {
   return (
     <div>
       <h1>D</h1>
-      <span>{props.time.h >= 0 ? props.time.h : "0" + props.time.h}</span>
+      <span>{props.time.h >= 10 ? props.time.h : "0" + props.time.h}</span>
       &nbsp;&nbsp;
-      <span>{props.time.m}</span>&nbsp;&nbsp;
-      <span>{props.time.s}</span>&nbsp;&nbsp;
-      <span>{props.time.ms}</span>&nbsp;&nbsp;
+      <span>{props.time.h >= 10 ? props.time.h : "0" + props.time.h}</span>
+      &nbsp;&nbsp;
+      <span>{props.time.h >= 10 ? props.time.h : "0" + props.time.h}</span>
+      &nbsp;&nbsp;
+      <span>{props.time.h >= 10 ? props.time.h : "0" + props.time.h}</span>
+      &nbsp;&nbsp;
     </div>
   )
 }
