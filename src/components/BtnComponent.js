5838ab70e52c694753a4f7783bc52d70d8446e81
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-22 11:14:30 +0300

    upgrade

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -1,12 +1,7 @@
-function BtnComponent(props) {
-  return (
-    <div className="main-section">
-      <span>{props.h}</span>&nbsp;&nbsp;
-      <span>{props.m}</span>&nbsp;&nbsp;
-      <span>{props.s}</span>&nbsp;&nbsp;
-      <span>{props.ms}</span>&nbsp;&nbsp;
-    </div>
-  )
+import React from "react"
+
+function BtnComponent() {
+  return <div className="main-section"></div>
 }
 
 export default BtnComponent
--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -1,7 +1,14 @@
-function DisplayComponent() {
+import React from "react"
+
+function DisplayComponent(props) {
   return (
     <div>
       <h1>D</h1>
+      <span>{props.time.h >= 0 ? props.time.h : "0" + props.time.h}</span>
+      &nbsp;&nbsp;
+      <span>{props.time.m}</span>&nbsp;&nbsp;
+      <span>{props.time.s}</span>&nbsp;&nbsp;
+      <span>{props.time.ms}</span>&nbsp;&nbsp;
     </div>
   )
 }
